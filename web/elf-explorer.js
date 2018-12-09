255c322b02c4beefbebbc4c44ab940158db7271d
Mustafa Serdar Sanli <mserdarsanli@gmail.com>
2018-12-09 05:00:46 +0000

    Move addPopup code to js file

--- web/elf-explorer.js
+++ web/elf-explorer.js
@@ -70,3 +70,56 @@ document.getElementById( 'drop-area' ).addEventListener( 'drop', function( ev ) 
   } );
 
 });
+
+
+
+function addPopup( ev ) {
+  ev.stopPropagation();
+  console.log( 'addpopup called with event: ', ev );
+
+  ev.target.style.position = 'relative';
+
+  d1 = document.createElement( 'div' );
+  d1.addEventListener( 'click', function( ev ) {
+    // Is there a better way to do this?
+    ev.stopPropagation();
+  } );
+  d1.innerHTML = '<div style="text-align: center; font-weight: bold; font-size: large;">SHT_RELA</div><p>Relocation entries with addends</p>';
+  d1.style.position = 'absolute';
+  d1.style.width = '300px';
+  d1.style.top = '0%';
+  d1.style.left = '50%';
+  d1.style.transform = 'translate(-50%, calc( -100% - 10px ) )';
+  d1.style.backgroundColor = '#eeeeee';
+  d1.style.border = '1px solid black';
+  d1.style.padding = '5px';
+
+  a = document.createElement( 'span' );
+  a.innerText = 'X';
+  a.style.fontWeight = 'bold';
+  a.style.backgroundColor = '#dddddd';
+  a.addEventListener( 'click', function( ev ) {
+    ev.stopPropagation();
+    ev.target.parentNode.parentNode.remove();
+  } );
+  a2 = document.createElement( 'div' );
+  a2.style.textAlign = 'right';
+  a2.appendChild( a );
+  d1.insertBefore( a2, d1.childNodes[0] );
+
+  tip = document.createElement( 'div' );
+  tip.style.border = '10px solid transparent';
+  tip.style.width = '0px';
+  tip.style.borderTopColor = 'black';
+  tip.style.position = 'absolute';
+  tip.style.top = '100%';
+  tip.style.left = '50%';
+  tip.style.transform = 'translate( -50%, 0% )';
+  d1.appendChild( tip );
+
+  ev.target.appendChild( d1 );
+}
+
+function testAddPopup() {
+  document.getElementsByTagName( 'abbr' )[4].addEventListener( 'click', addPopup );
+}
